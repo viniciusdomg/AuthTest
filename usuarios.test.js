4952a4cbc0bd34674ddbf7b692c9ab1a219f93d7
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2024-08-05 10:07:22 -0300

    Testando autenticação do usuário

--- usuarios.test.js
+++ usuarios.test.js
@@ -53,7 +53,7 @@ describe("Senha com apenas maiúsculas sem 8 caracteres", () => {
 });
 
 describe("Senha com apenas 8 caracteres", () => {
-  test("Verificar senha média", () => {
+  test("Verificar senha muito fraca", () => {
     const resultado = validateUserAndPassword("Usuario", "12345678");
     expect(resultado).toEqual({
       message:
@@ -75,6 +75,91 @@ describe("Senha com maiúsculas e minusculas sem 8 caracteres", () => {
     });
   });
 });
+
+describe("Senha com minusculas e caracter especial sem 8 caracteres", () => {
+  test("Verificar senha fraca", () => {
+    const resultado = validateUserAndPassword("Usuario", "mateus@");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Fraca",
+      valid: false,
+    });
+  });
+});
+
+describe("Senha com maiusculas e caracter especial sem 8 caracteres", () => {
+  test("Verificar senha fraca", () => {
+    const resultado = validateUserAndPassword("Usuario", "MATEUS@");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Fraca",
+      valid: false,
+    });
+  });
+});
+
+describe("Senha com 8 caracteres e caracter especial", () => {
+  test("Verificar senha fraca", () => {
+    const resultado = validateUserAndPassword("Usuario", "12345678@");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Fraca",
+      valid: false,
+    });
+  });
+});
+
+describe("Senha com 8 caracteres e letras minusculas", () => {
+  test("Verificar senha média", () => {
+    const resultado = validateUserAndPassword("Usuario", "matheus12345678");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Fraca",
+      valid: false,
+    });
+  });
+});
+
+describe("Senha com 8 caracteres e letras maiusculas", () => {
+  test("Verificar senha média", () => {
+    const resultado = validateUserAndPassword("Usuario", "MATHEUS12345678");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Fraca",
+      valid: false,
+    });
+  });
+});
+
+describe("Senha com minusculas e caracter especial com 8 caracteres", () => {
+  test("Verificar senha média", () => {
+    const resultado = validateUserAndPassword("Usuario", "matheus@");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Média",
+      valid: false,
+    });
+  });
+});
+
+describe("Senha com maiusculas e caracter especial com 8 caracteres", () => {
+  test("Verificar senha média", () => {
+    const resultado = validateUserAndPassword("Usuario", "MATHEUS@");
+    expect(resultado).toEqual({
+      message:
+        "Senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.",
+      strength: "Média",
+      valid: false,
+    });
+  });
+});
+
 
 describe("Senha com maiusculas, minusculas e caracter especial sem 8 caracteres", () => {
   test("Verificar senha média", () => {
